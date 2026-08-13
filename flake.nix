fa6d351f2c4d4617c93481054e9724fb4e97cefa
zaeem <zaeemali272@gmail.com>
2026-08-13 19:16:34 +0500

    refactor: remove automatic zenith-shell configuration file copying from home-manager module

--- flake.nix
+++ flake.nix
@@ -93,14 +93,6 @@
 
           config = lib.mkIf cfg.enable {
             home.packages = [ cfg.package ];
-            
-            # Automatically copy/sync zenith-shell files to ~/.config/quickshell (only if not already a symlink)
-            home.activation.copyZenithShell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-              if [ ! -L "$HOME/.config/quickshell" ]; then
-                $DRY_RUN_CMD mkdir -p $HOME/.config/quickshell
-                $DRY_RUN_CMD cp -rL --no-preserve=mode ${cfg.package}/share/zenith-shell/* $HOME/.config/quickshell/
-              fi
-            '';
           };
         };
     };
